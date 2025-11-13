37492392743880ed5134bf698f5373b155bb3b93
prithvi <prithvi@cloudera.com>
2025-11-14 03:51:29 +0530

    FIX: remove deprecated pandas CSV reader args (keep_date_col, verbose, delim_whitespace)
    Signed-off-by: prithvi <prithvi@cloudera.com>

--- hamilton/plugins/pandas_extensions.py
+++ hamilton/plugins/pandas_extensions.py
@@ -205,14 +205,10 @@ class PandasCSVReader(DataLoader):
             kwargs["keep_default_na"] = self.keep_default_na
         if self.na_filter is not None:
             kwargs["na_filter"] = self.na_filter
-        if self.verbose is not None:
-            kwargs["verbose"] = self.verbose
         if self.skip_blank_lines is not None:
             kwargs["skip_blank_lines"] = self.skip_blank_lines
         if self.parse_dates is not None:
             kwargs["parse_dates"] = self.parse_dates
-        if self.keep_date_col is not None:
-            kwargs["keep_date_col"] = self.keep_date_col
         if self.date_format is not None:
             kwargs["date_format"] = self.date_format
         if self.dayfirst is not None:
@@ -247,8 +243,6 @@ class PandasCSVReader(DataLoader):
             kwargs["dialect"] = self.dialect
         if self.on_bad_lines is not None:
             kwargs["on_bad_lines"] = self.on_bad_lines
-        if self.delim_whitespace is not None:
-            kwargs["delim_whitespace"] = self.delim_whitespace
         if self.low_memory is not None:
             kwargs["low_memory"] = self.low_memory
         if self.memory_map is not None:
