0f4678bef28fd1d8ffaf2726408b313af8c5fe49
g97iulio1609 <giulio97.leone@gmail.com>
2026-02-28 06:20:23 +0100

    perf: use deque for BFS queue in graph traversal
    compute_nodes_from_sources() drains a BFS queue via .pop(0) which is
    O(n) per removal.  Switch to collections.deque with .popleft() for
    O(1) front removal.

--- hamilton/execution/graph_functions.py
+++ hamilton/execution/graph_functions.py
@@ -16,6 +16,7 @@
 # under the License.
 
 import logging
+from collections import deque
 import pprint
 from collections.abc import Collection
 from functools import partial
@@ -65,12 +66,12 @@ def topologically_sort_nodes(nodes: list[node.Node]) -> list[node.Node]:
     in_degrees = {node_.name: len(dependency_map.get(node_.name, [])) for node_ in nodes}
     # TODO -- determine what happens if nodes have dependencies that aren't present
     sources = [node_ for node_ in nodes if in_degrees[node_.name] == 0]
-    queue = []
+    queue = deque()
     for source in sources:
         queue.append(source)
     sorted_nodes = []
     while len(queue) > 0:
-        node_ = queue.pop(0)
+        node_ = queue.popleft()
         sorted_nodes.append(node_)
         for next_node in depended_on_by_map.get(node_.name, []):
             if next_node.name in in_degrees:
