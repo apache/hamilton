ac27b34450bd9aea39aceb0b6fea58de1f4058a0
Stefan Krawczyk <stefank@cs.stanford.edu>
2026-03-08 09:05:09 -0700

    Add deprecation decorators to telemetry stub functions
    Mark disable_telemetry() and is_telemetry_enabled() as deprecated
    (warn at 1.89.0, fail at 2.0.0) so users get a clear signal to
    remove these calls before the next major release.

--- hamilton/telemetry.py
+++ hamilton/telemetry.py
@@ -23,12 +23,28 @@ so that any user code calling ``telemetry.disable_telemetry()``
 will not break.
 """
 
+from hamilton.dev_utils.deprecation import deprecated
 
+
+@deprecated(
+    warn_starting=(1, 89, 0),
+    fail_starting=(2, 0, 0),
+    use_this=None,
+    explanation="Telemetry has been removed from Hamilton. This function is a no-op.",
+    migration_guide="Simply remove any calls to `telemetry.disable_telemetry()`.",
+)
 def disable_telemetry():
     """No-op. Telemetry has been removed."""
     pass
 
 
+@deprecated(
+    warn_starting=(1, 89, 0),
+    fail_starting=(2, 0, 0),
+    use_this=None,
+    explanation="Telemetry has been removed from Hamilton. This function always returns False.",
+    migration_guide="Simply remove any calls to `telemetry.is_telemetry_enabled()`.",
+)
 def is_telemetry_enabled() -> bool:
     """Always returns False. Telemetry has been removed."""
     return False
